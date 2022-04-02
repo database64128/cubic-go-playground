4ec0d29e8df06a1265fbcec92832e5499e32e534
database64128 <free122448@hotmail.com>
2022-04-03 04:41:58 +0800

    🚿 CSPRNG benchmarks: Add stream ciphers

--- csprng/csprng_test.go
+++ csprng/csprng_test.go
@@ -1,10 +1,13 @@
 package csprng
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"io"
 	"testing"
 
+	"golang.org/x/crypto/chacha20"
 	"lukechampine.com/blake3"
 )
 
@@ -75,5 +78,112 @@ func BenchmarkBlake3KeyedHashBig(b *testing.B) {
 		if err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+func initAesCtr(b *testing.B, keySize int) cipher.Stream {
+	key := make([]byte, keySize)
+	_, err := rand.Read(key)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	cb, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	iv := make([]byte, cb.BlockSize())
+	_, err = rand.Read(iv)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	return cipher.NewCTR(cb, iv)
+}
+
+func BenchmarkAes128CtrSmall(b *testing.B) {
+	buf := make([]byte, testSmallSize)
+	cs := initAesCtr(b, 16)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		cs.XORKeyStream(buf, buf)
+	}
+}
+
+func BenchmarkAes128CtrBig(b *testing.B) {
+	buf := make([]byte, testBigSize)
+	cs := initAesCtr(b, 16)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		cs.XORKeyStream(buf, buf)
+	}
+}
+
+func BenchmarkAes256CtrSmall(b *testing.B) {
+	buf := make([]byte, testSmallSize)
+	cs := initAesCtr(b, 32)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		cs.XORKeyStream(buf, buf)
+	}
+}
+
+func BenchmarkAes256CtrBig(b *testing.B) {
+	buf := make([]byte, testBigSize)
+	cs := initAesCtr(b, 32)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		cs.XORKeyStream(buf, buf)
+	}
+}
+
+func initChaCha20(b *testing.B) cipher.Stream {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	nonce := make([]byte, chacha20.NonceSize)
+	_, err = rand.Read(nonce)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	cs, err := chacha20.NewUnauthenticatedCipher(key, nonce)
+	if err != nil {
+		b.Fatal(err)
+	}
+	return cs
+}
+
+func BenchmarkChaCha20Small(b *testing.B) {
+	buf := make([]byte, testSmallSize)
+	cs := initChaCha20(b)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		cs.XORKeyStream(buf, buf)
+	}
+}
+
+func BenchmarkChaCha20Big(b *testing.B) {
+	buf := make([]byte, testBigSize)
+	cs := initChaCha20(b)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		cs.XORKeyStream(buf, buf)
 	}
 }
